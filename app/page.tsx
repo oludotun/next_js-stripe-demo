92633797797a8ca6e764ad4662f9410f617a08d5
Oludotun Ebiekuraju <dotun@intuitigo.com>
2025-03-27 19:15:47 +0100

    Add cancellation notification for Stripe checkout in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
+"use client";
+
 import Image from "next/image";
+import * as React from "react";
+import { useSearchParams } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Home() {
+    const searchParams = useSearchParams();
+
+    React.useEffect(() => {
+        const timer = setTimeout(() => {
+            if (searchParams.has("canceled")) {
+                toast(
+                    "Order canceled -- continue to shop around and checkout when you're ready."
+                );
+            }
+        }, 0);
+
+        return () => clearTimeout(timer);
+    }, [searchParams]);
+
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -46,15 +65,17 @@ export default function Home() {
                         href={process.env.NEXT_PUBLIC_PAYMENT_LINK}
                         rel="noopener noreferrer"
                     >
-                        {/* <Image
-                            className="dark:invert"
-                            src="/vercel.svg"
-                            alt="Vercel logomark"
-                            width={20}
-                            height={20}
-                        /> */}
-                        Pay now
+                        Pay with Payment Link
                     </a>
+                    <form action="/api/checkout_sessions" method="POST">
+                        <button
+                            type="submit"
+                            role="link"
+                            className="cursor-pointer rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+                        >
+                            Pay Now (Checkout)
+                        </button>
+                    </form>
                 </div>
             </main>
             <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
