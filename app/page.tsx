4acfcbcd9de141b6caaeb815cb040efef305d4c1
Oludotun Ebiekuraju <dotun@intuitigo.com>
2025-03-27 17:02:56 +0100

    Update payment link to use environment variable for dynamic configuration

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
                 <div className="flex gap-4 items-center flex-col sm:flex-row">
                     <a
                         className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-                        href="https://buy.stripe.com/test_eVag2w0io7ITbFC288"
+                        href={process.env.NEXT_PUBLIC_PAYMENT_LINK}
                         rel="noopener noreferrer"
                     >
                         {/* <Image
