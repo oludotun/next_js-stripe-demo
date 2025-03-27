1f20a761f46ca5d85ecbd6fe9fec1b18b580ecac
Oludotun Ebiekuraju <dotun@intuitigo.com>
2025-03-27 16:58:25 +0100

    Add Stripe webhook handler for processing events

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRETE_KET!);
+export async function POST(req: NextRequest) {
+    const payload = await req.text();
+    const res = JSON.parse(payload);
+
+    const sig = req.headers.get("stripe-signature");
+
+    try {
+        const event = stripe.webhooks.constructEvent(
+            payload,
+            sig!,
+            process.env.STRIPE_WEBHOOK_SECRETE!
+        );
+        const obj = res?.data?.object;
+        const responseData = {
+            email: obj?.billing_details?.email,
+            amount: obj?.amount,
+            date: new Date(res?.created * 1000).toLocaleDateString(),
+            details: JSON.stringify(res),
+        };
+        // Send this to the database instead of login it
+        console.log(responseData);
+
+        return NextResponse.json({
+            status: "success",
+            event: event.type,
+        });
+    } catch (err) {
+        return NextResponse.json({ status: "webhook_error", err });
+    }
+}
