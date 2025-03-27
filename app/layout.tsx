42a7600d5d614aac6ead97ca45e574f9400ae1d2
Oludotun Ebiekuraju <dotun@intuitigo.com>
2025-03-27 20:04:18 +0100

    Wrap Home component in Suspense to fix compiling issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Suspense } from "react";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
@@ -28,8 +29,10 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                {children}
-                <Toaster />
+                <Suspense>
+                    {children}
+                    <Toaster />
+                </Suspense>
             </body>
         </html>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import * as React from "react";
 import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
 import { toast } from "sonner";
 
 export default function Home() {
@@ -22,112 +21,110 @@ export default function Home() {
     }, [searchParams]);
 
     return (
-        <Suspense>
-            <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-                <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                    <a href="https://github.com/oludotun">
-                        <Image
-                            className="dark:invert rounded-full"
-                            src="https://github.com/oludotun.png"
-                            alt="Next.js logo"
-                            width={90}
-                            height={90}
-                            priority
-                        />
-                    </a>
-                    <h1 className="text-3xl">
-                        NextJS Stripe Payment Integration Demo
-                    </h1>
-                    <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-                        <li className="mb-2 tracking-[-.01em]">
-                            See Code on GitHub{" "}
-                            <a href="https://github.com/oludotun/next_js-stripe-demo">
-                                <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-                                    https://github.com/oludotun/next_js-stripe-demo
-                                </code>
-                            </a>
-                            .
-                        </li>
-                        <li className="tracking-[-.01em]">
-                            Click <span className="font-semibold">Pay Now</span>{" "}
-                            to check it out
-                        </li>
-                        <li className="tracking-[-.01em]">
-                            <span className="font-semibold">
-                                Please Don&apos;t use your real card
-                            </span>{" "}
-                            You can use 4242424242424242 with any valid future
-                            date and any three-digit CVC
-                        </li>
-                    </ol>
-
-                    <div className="flex gap-4 items-center flex-col sm:flex-row">
-                        <a
-                            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-                            href={process.env.NEXT_PUBLIC_PAYMENT_LINK}
-                            rel="noopener noreferrer"
-                        >
-                            Pay with Payment Link
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                <a href="https://github.com/oludotun">
+                    <Image
+                        className="dark:invert rounded-full"
+                        src="https://github.com/oludotun.png"
+                        alt="Next.js logo"
+                        width={90}
+                        height={90}
+                        priority
+                    />
+                </a>
+                <h1 className="text-3xl">
+                    NextJS Stripe Payment Integration Demo
+                </h1>
+                <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+                    <li className="mb-2 tracking-[-.01em]">
+                        See Code on GitHub{" "}
+                        <a href="https://github.com/oludotun/next_js-stripe-demo">
+                            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+                                https://github.com/oludotun/next_js-stripe-demo
+                            </code>
                         </a>
-                        <form action="/api/checkout_sessions" method="POST">
-                            <button
-                                type="submit"
-                                role="link"
-                                className="cursor-pointer rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-                            >
-                                Pay Now (Checkout)
-                            </button>
-                        </form>
-                    </div>
-                </main>
-                <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+                        .
+                    </li>
+                    <li className="tracking-[-.01em]">
+                        Click <span className="font-semibold">Pay Now</span> to
+                        check it out
+                    </li>
+                    <li className="tracking-[-.01em]">
+                        <span className="font-semibold">
+                            Please Don&apos;t use your real card
+                        </span>{" "}
+                        You can use 4242424242424242 with any valid future date
+                        and any three-digit CVC
+                    </li>
+                </ol>
+
+                <div className="flex gap-4 items-center flex-col sm:flex-row">
                     <a
-                        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
+                        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+                        href={process.env.NEXT_PUBLIC_PAYMENT_LINK}
                         rel="noopener noreferrer"
                     >
-                        <Image
-                            aria-hidden
-                            src="/file.svg"
-                            alt="File icon"
-                            width={16}
-                            height={16}
-                        />
-                        Learn
+                        Pay with Payment Link
                     </a>
-                    <a
-                        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            aria-hidden
-                            src="/window.svg"
-                            alt="Window icon"
-                            width={16}
-                            height={16}
-                        />
-                        Examples
-                    </a>
-                    <a
-                        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                        href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            aria-hidden
-                            src="/globe.svg"
-                            alt="Globe icon"
-                            width={16}
-                            height={16}
-                        />
-                        Go to nextjs.org →
-                    </a>
-                </footer>
-            </div>
-        </Suspense>
+                    <form action="/api/checkout_sessions" method="POST">
+                        <button
+                            type="submit"
+                            role="link"
+                            className="cursor-pointer rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+                        >
+                            Pay Now (Checkout)
+                        </button>
+                    </form>
+                </div>
+            </main>
+            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+                <a
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/file.svg"
+                        alt="File icon"
+                        width={16}
+                        height={16}
+                    />
+                    Learn
+                </a>
+                <a
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/window.svg"
+                        alt="Window icon"
+                        width={16}
+                        height={16}
+                    />
+                    Examples
+                </a>
+                <a
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/globe.svg"
+                        alt="Globe icon"
+                        width={16}
+                        height={16}
+                    />
+                    Go to nextjs.org →
+                </a>
+            </footer>
+        </div>
     );
 }
